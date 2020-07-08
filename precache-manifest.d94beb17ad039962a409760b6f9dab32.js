4b4c42aaed7a863d241f79de120cfa670c8cdedd
A. Pretti <48262876+APretti789@users.noreply.github.com>
2020-07-08 00:05:46 -0300

    Updates

---
+++ precache-manifest.d94beb17ad039962a409760b6f9dab32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da016de176892fa083e6f2bcee22a03",
+    "url": "/ReactCourseProj1/index.html"
+  },
+  {
+    "revision": "dee8e35ee4984a02f9aa",
+    "url": "/ReactCourseProj1/static/css/main.8e02b50e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/ReactCourseProj1/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCourseProj1/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee8e35ee4984a02f9aa",
+    "url": "/ReactCourseProj1/static/js/main.71c4fef6.chunk.js"
+  },
+  {
+    "revision": "9f8b3b64de4c64859b55",
+    "url": "/ReactCourseProj1/static/js/runtime-main.4e6fe9de.js"
+  }
+]);
